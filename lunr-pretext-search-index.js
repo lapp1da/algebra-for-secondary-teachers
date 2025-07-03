4d30f5a4a4de40cba197af686f57b981319c309d
Douglas A. Lapp <lapp1da@cmich.edu>
2025-07-03 22:10:14 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,104 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "frontmatter-4",
+  "level": "1",
+  "url": "frontmatter-4.html",
+  "type": "Dedication",
+  "number": "",
+  "title": "Dedication",
+  "body": " I want to dedicate this text to my wonderful wife, Kim, whose sacrifices and support have always made my professional goals possible. She was willing to limit her own professional ambitions to accommodate my needs. Thank you and I love you!  I also want to thank my kids, Noah, Ellen, and Sarah, for being my inspiration in the teaching and learning of mathematics. For two of them, I had the pleasure of actually being their professor and they have given me insights into how students learn mathematics. They all have been supportive of my professional life. I love you all!  "
+},
+{
+  "id": "frontmatter-5",
+  "level": "1",
+  "url": "frontmatter-5.html",
+  "type": "Acknowledgements",
+  "number": "",
+  "title": "Acknowledgements",
+  "body": " This book is an application of research I have conducted on the teaching and learning of algebra at both the K-12 and the university levels. My work was influenced by many scholars, but in particular, the work of the late Dr. Jim Kaput, as well as Dr. Kathy Heid, Dr. Rose Zbiek, and Dr. Anna Sfard stand out as those who have profoundly shaped my thinking.  "
+},
+{
+  "id": "sec11-brief-history-standards",
+  "level": "1",
+  "url": "sec11-brief-history-standards.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "A Brief History of Standards for Mathematics Education: Algebraic Perspectives",
+  "body": " A Brief History of Standards for Mathematics Education: Algebraic Perspectives   Although the Principle and Standards for School Mathematics (PSSM) (NCTM, 2000) and the Common Core State Standards (NGACBP and CCSSO, 2010) treat algebra as a standard for PreK-12, society tends to view algebra as a part of mathematics relegated to the secondary curriculum. In fact, even mathematicians often fail to understand how algebra could be taught to a first grader. Why is there such a misunderstanding between organizations such as NCTM and society? Why is there such a misunderstanding between NCTM and the mathematics community? Perhaps in order to understand why these differences in the perception of algebra exist, we must first look at the historical and traditional teaching of algebra in the United States.   Suppose you were to ask the person on the street to describe algebra. What sort of response would you get? What about mathematics in general? This question was asked of a student from a Minnesota high school during ABC's Primetime Live following the release of the results from the Third International Mathematics and Science Study (ABC News, 1998). In response to this question she replied, “It's kind of a set in stone. It's procedure. It's a formula. You learn the formula and you know how to do math.”  Her perceptions of mathematics and, in particular, algebra, are not unique. In fact, the argument could be made that this is the perception of the majority of people—even those who are well educated. So, we must ask the question of why do so many people view mathematics as a set of procedures to be memorized? This question is of particular interest as we explore the teaching and learning of algebra since so many algebra texts are structured in such a way as to perpetuate this view.  Think back to your own algebra courses. What were the items you remember most from your textbooks? Can you recall shaded boxes housing formulae and “rules”? Do you recall the “Laws of Exponents” prominently displayed on the page? Did you ever think to question them? Or did you ever wonder why they were presented in the text before any examples like were even discussed? Maybe this is the reason society holds a very procedural and limited view of algebra.  In this text, we will explore not only the teaching and learning of algebra, but also the very nature of this branch of mathematics. If we wish to understand how students learn mathematics, we must first ask questions about the nature of the mathematics to be learned. If mathematics indeed is a set of procedures to be memorized as suggested in the quote given above, then there are certain strategies that can be employed to facilitate the memorization. This would then be an appropriate method of teaching. However, if the nature of mathematics is far more than the memorization of a set of rules, the strategies employed for learning mathematics will be completely different from those employed for memorization.  Traditionally, mathematical content knowledge and pedagogical content knowledge have been the main parts of a dichotomous preparation program for teachers. Research suggests that this approach to the preparation of teachers is inadequate. How can we separate mathematics and the teaching and learning of mathematics? Is it possible to learn how to teach devoid of specific content knowledge? If so, then this would suggest that students learn language, social studies, art, science, and mathematics all the same way. The literature suggests otherwise. So, if students learn different subjects differently, why do we tend to separate the mathematical content for preservice teachers from the pedagogical content? It is our opinion that both mathematical content knowledge and pedagogical content knowledge need to develop simultaneously. For this reason, we will take a unified approach to integrate mathematical learning with the theories and practice for teaching.  "
+},
+{
+  "id": "sec12-ccss-content-overview",
+  "level": "1",
+  "url": "sec12-ccss-content-overview.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Common Core State Standards for Mathematics: Content Standards",
+  "body": " Common Core State Standards for Mathematics: Content Standards  Text of section.  "
+},
+{
+  "id": "sec13-ccss-mathematical-practice",
+  "level": "1",
+  "url": "sec13-ccss-mathematical-practice.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Common Core State Standards for Mathematics: Standards for Mathematical Practice",
+  "body": " Common Core State Standards for Mathematics: Standards for Mathematical Practice  Text of section.  "
+},
+{
+  "id": "sec21-elementary-concepts-number",
+  "level": "1",
+  "url": "sec21-elementary-concepts-number.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Elementary Concepts of Number",
+  "body": " Elementary Concepts of Number  Text of section.  "
+},
+{
+  "id": "sec22-complex-numbers",
+  "level": "1",
+  "url": "sec22-complex-numbers.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Extending the Number System: Complex Numbers",
+  "body": " Extending the Number System: Complex Numbers  Text of section.  "
+},
+{
+  "id": "sec23-intro-vectors-matrices",
+  "level": "1",
+  "url": "sec23-intro-vectors-matrices.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Introduction to Vectors and Matrices",
+  "body": " Introduction to Vectors and Matrices  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
